10008fab49b159d41604c6693f5d5f2e459a1739
Ikuti <m.j.olkiewicz@gmail.com>
2021-06-20 18:14:38 +0200

    added id and setup it up for planets

--- Game/Scripts/Planet.cs
+++ Game/Scripts/Planet.cs
@@ -3,6 +3,7 @@ using System;
 
 public class Planet : Node
 {
+    [Export] public int id;
     [Export] public string name;
     [Export] public string pathToImage;
     [Export(PropertyHint.MultilineText)] public string importantPoints;
